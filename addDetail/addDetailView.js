b9257c754c5174683fc6a4112f6f05c019fe90a9
Javier Girón <countryroud@gmail.com>
2023-03-12 05:21:34 +0100

    CSS AddDetail Ok

--- addDetail/addDetailView.js
+++ addDetail/addDetailView.js
@@ -1,26 +1,27 @@
 export function buildAddDetail(product) {
     return `
     <div class="add">
-        <div class="productInfo> 
+        <div class="productInfo"> 
             <div class="userInfo">
-                <span><img src="${product.avatar}" alt="Avatar del usuario"> - ${product.username}</span>
+                <img src="${product.avatar}" alt="Avatar del usuario">
+                <p>${product.username}</p>
             </div>
             <div class="productName">
                 <h1>${product.name}</h1>
+                <p>Fecha de publicación: </p>
             </div>
+            <div class="productImg">
+                <img src="${product.photo}">            
+                <p>Tags: <span>${product.tag.join(', ')}.</span> </p>
+            </div>
+            </div>
+            <div class="description">
+            <h3 class="descriptionTittle">Descripción:<h3>
+            <p>${product.description}</p>
             <div class = "productData">
                 <p> Se <span id="isSale" >${product.sale}</span> este producto por:</p>
                 <h2>${product.price} €</h2>
                 <div class = "typeTag">
-                    <p>Tags: <span>${product.tag.join(', ')}.</span> </p>
-                </div>
-                <div class="description">
-                    <h3 class="descriptionTittle">Descripción:<h3>
-                    <p>${product.description}</p>
-            </div>
-            <div class="product-img">
-                <img src="${product.photo}">            
-                <p>Fecha de creación: </p>
             </div>
         </div>
         <button id="deleteAdd">Delete Add</button>
